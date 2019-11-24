4af5355315deb79a2956e3881f1b66680d5f48f1
onur onur2503 <onur2503@outlook.com>
2019-11-24 21:22:17 +0300

    004

--- main.c
+++ main.c
@@ -1,20 +1,59 @@
-// soruBankasi.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<stdio.h>
+#include<conio.h> // getche için
 
-#include <iostream>
+
+void soru_ekle(); //soru ekleme fonksiyon tanýmý
+int menu();       // menu fonksiyonunun tanýmý
+
+
+
+char secim;
+char sorular[15];
+
 
 int main()
+
 {
-    std::cout << "Hello World!\n";
+
+	menu(); // menu için fonksiyon oluþturup burada çaðýrdýk
+	switch (secim)
+	{
+	case '1':
+	{
+		soru_ekle();
+
+	}
+	break;
+	}
+
+
+
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int menu()//menu fonksiyonu
+{
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	printf("1. Soru ekle\n");
+	printf("2. Soru sil\n");
+	printf("3. Sorulari goster ve guncelle\n");
+	printf("4. Oyun oyna\n");
+	printf("5. En yuksek skorlari goster\n");
+	printf("6. Cik\n");
+	printf("seciminiz: ");
+	secim = getche(); // char ve getche kullanmamým sebebi; int ve scanf ile alýnca entere basmak gerekiyor öyle olunca soru ekleme iþlemi yapamadan program sonlanýyo.
+
+	return 'secim';
+
+}
+void soru_ekle()
+{
+	FILE* dosya; // dosya için
+	dosya = fopen("sorular.txt", "w"); // sorular.txt adýnda belge oluþturur w: okuma ve yazma iþlemi yapýlabilir dosya
+	printf("\n soru1:");
+	gets(sorular); // soru giriþi alýyoruz
+
+	fprintf(dosya, "%s", sorular);// dosyaya yazýyor
+	fclose(dosya);//dosyayý kapatýyor
+}
