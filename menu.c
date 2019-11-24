8803f097183c7154e7f7c6f438ea95770794bc50
onur onur2503 <onur2503@outlook.com>
2019-11-24 21:46:23 +0300

    001

--- menu.c
+++ menu.c
@@ -1,3 +1,35 @@
+#include<stdio.h>
+#include<conio.h> // getche için
+
+
+int soru_ekle(); //soru ekleme fonksiyon tanýmý
+int menu();       // menu fonksiyonunun tanýmý
+int secim;
+
+char sorular[15];
+
+int main()
+
+{
+
+	menu(); // menu için fonksiyon oluþturup burada çaðýrdýk
+
+	switch (secim)
+	{
+	case '1':
+	{
+		soru_ekle();
+
+	}
+	break;
+	}
+
+
+
+
+	return 0;
+}
+
 int menu()//menu fonksiyonu
 {
 
@@ -8,8 +40,19 @@ int menu()//menu fonksiyonu
 	printf("5. En yuksek skorlari goster\n");
 	printf("6. Cik\n");
 	printf("seciminiz: ");
-	int secim = getche(); // char ve getche kullanmamým sebebi; int ve scanf ile alýnca entere basmak gerekiyor öyle olunca soru ekleme iþlemi yapamadan program sonlanýyo.
+	secim = getche(); // char ve getche kullanmamým sebebi; int ve scanf ile alýnca entere basmak gerekiyor öyle olunca soru ekleme iþlemi yapamadan program sonlanýyo.
 
 	return secim;
 
+}
+int soru_ekle()
+{
+	FILE* dosya; // dosya için
+	dosya = fopen("sorular.txt", "w"); // sorular.txt adýnda belge oluþturur w: okuma ve yazma iþlemi yapýlabilir dosya
+	printf("\n soru1:");
+	gets(sorular); // soru giriþi alýyoruz
+
+	fprintf(dosya, "%s", sorular);// dosyaya yazýyor
+	fclose(dosya);//dosyayý kapatýyor
+	return 0;
 }
